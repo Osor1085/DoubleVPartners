51b84446d2a3db4d7c7a59e52b04fa13f7104b7e
Osor1085 <49964719+Osor1085@users.noreply.github.com>
2023-03-08 02:11:00 -0500

    Commit escenarios autmatizados

---
+++ sample_spec2.cy.js
@@ -0,0 +1,6 @@
+describe('PRUEBA DOUBLE V PARTNERS', () => {
+  it('Prueba 02 - Visita pagina BOKKINK WING', () => {
+    cy.visit('https://booking.wingo.com/es/search/BOG/CLO/2023-03-09/2023-04-09/2/0/0/0/COP/0/0')
+    
+  })
+})
